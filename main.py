e21ae57fa08d31b61c6c1f58d26e6a713ddbea17
TouchIsNotAGoodCoder <64277067+Touchcreator@users.noreply.github.com>
2022-07-19 11:24:54 -0400

    0.0.2.1| WoW cOoL cOmMeNtS aNd ReAd CoMmAnD!!!1!!!

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-import time, os
+import time, os, platform
+
+plat = platform.system() # get the os
+archie = platform.machine() # get the architeture
+
+if (plat!="Windows"): # inform the user that touch os was made for windows
+    useanyway = input("Your host OS was detected as " + plat + ". However, Touch OS was designed for Windows. Use anyway? ")
+    if (useanyway == "n"):
+        print("Ok then, exiting")
+        time.sleep(1)
+        exit()
 
 print("""
  ______________________
-|    Touch OS 0.0.1    |
+|   Touch OS 0.0.2.1   |
 |                      |
 |    POV: Not an OS    |
 |______________________|
 """)
 
-da_commands = ["exit", "gotodir", "clear", "host", "say", "run", "passout", "help"]
+da_commands = ["exit", "gotodir", "clear", "host", "say", "run", "passout", "help", "read"]
 def read_cmd(cmd):
     if cmd == "exit":
         exit()
@@ -16,7 +26,8 @@ def read_cmd(cmd):
         print(os.listdir(cwd))
     elif (cmd.__contains__("gotodir ")):
         try:
-            os.chdir(cmd[8:len(cmd)])
+            os.chdir(cmd[8:len(cmd)]) # chdirs to the listed dir
+        # any errors vvvvvvvvvvvvvvvvvvvvvvvvvvv
         except FileNotFoundError:
             print("That folder doesn't exist")
         except NotADirectoryError:
@@ -24,20 +35,35 @@ def read_cmd(cmd):
         except PermissionError:
             print("Cannot access that folder right now")
     elif cmd == "clear":
-        os.system("cls")
+        os.system("cls") # cls means clear
     elif (cmd.__contains__("host ")):
-        os.system(cmd[5:len(cmd)])
+        os.system(cmd[5:len(cmd)]) # does the listed command
     elif (cmd.__contains__("say ")):
-        print(cmd[4:len(cmd)])
+        print(cmd[4:len(cmd)]) # prints what you want it to print
     elif (cmd.__contains__("run ")):
         filepath = cmd[4:len(cmd)]
-        with open(filepath) as fp:
-            for index, line in enumerate(fp):
-                read_cmd(line.strip())
+        try:
+            with open(filepath) as fp:
+                for index, line in enumerate(fp):
+                    read_cmd(line.strip()) # reads the file, then reads the command on there
+        except FileNotFoundError:
+            print("That file doesn't exist")
+        except PermissionError:
+            print("Cannot access that folder right now")
     elif (cmd.__contains__("passout ")):
         time.sleep(int(cmd[8:len(cmd)]))
     elif cmd == "help":
         print(da_commands)
+    elif (cmd.__contains__("read ")): # literally just the run command
+        filepath = cmd[5:len(cmd)]
+        try:
+            with open(filepath) as fp:
+                for index, line in enumerate(fp):
+                    print(line.strip())
+        except FileNotFoundError:
+            print("That file doesn't exist")
+        except PermissionError:
+            print("Cannot access that folder right now")
 
     else:
         print("That command doesn't exist")
